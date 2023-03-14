e68ad8e20f1d96376c2d2e0c1fb250d4cb28abc8
francesco russo <frussohd@gmail.com>
2023-03-14 13:27:30 -0400

    home-update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,29 @@ export default function Home() {
     return(
         <>
             <div className="pageContainer">
-                <h2>Discuss Your Favorite Books</h2>
-                <p>Bookworm is a social platform where you can join discussions about your favorite books.</p>
+                <h2>Track Your Reading</h2>
+                <p>You finally have an all-in-one place to track the books you read over the years.</p>
             </div>
             <br />
             <div className="pageContainer">
-                <h2>Keep Handy Reading Notes</h2>
-                <p>Conveniently keep track of your thoughts and ideas as you read.</p>
+                <h2>Keep a Bank of Reading Notes</h2>
+                <p>Refresh yourself on the main points and takeaways of a book you read months or years ago. Hardcover is your convenient backlog of relevant thoughts that arise while reading.</p>
             </div>
+            <br />
+            <div className="pageContainer">
+                <h2>Find Your Next Book</h2>
+                <p>Our algorithm leverages the reading habits of our extensive userbase to assist you in finding your next book.</p>
+            </div>
+            <br />
+            <div className="pageContainer">
+                <h2>Discuss Your Favorite Books</h2>
+                <p>Hardcover is the perfect social platform for spirited discussions revolving around your favorite books.</p>
+            </div>
+            {/* <br />
+            <div className="pageContainer">
+                <h2>Start a Private Book Club</h2>
+                <p>Host your private book club to discuss a new book as a group.</p>
+            </div> */}
         </>
     )
 }
