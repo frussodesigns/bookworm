7a6de72a92d1d89c323b6b00c788ae5ef8c1ea02
francesco russo <frussohd@gmail.com>
2023-04-06 21:57:11 -0400

    keyboard visible bug 4

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -208,7 +208,7 @@ export default function RootLayout() {
                 <div className="mobileSpacer" />
             </main>
 
-            {keyVis == false || undefined &&
+            {!keyVis &&
             <div className="mobileNav">
             <div className="mobileNavFlex">
                 <NavLink 
